b6bc94c3c961c1c79e7e3f198a444288c3c7cf9d
Daniel K. Ayivon <84874876+naisyle@users.noreply.github.com>
2025-03-26 23:58:50 +0100

    Time: 0 ms (100.00%) | Memory: 42.2 MB (8.61%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.java
@@ -0,0 +1,44 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        // create an empty array of a * b of int[a][b]
+        // while the matrix is not empty 
+        // go through the top from left to right
+        // go down from top to bottom on the right
+        // go left to right on the bottom 
+        // go from bottom to top on the left 
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Top row
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[top][i]);
+            }
+            top++;
+
+            // Right column
+            for (int i = top; i <= bottom; i++) {
+                res.add(matrix[i][right]);
+            }
+            right--;
+
+            if (top <= bottom) {
+                // Bottom row
+                for (int i = right; i >= left; i--) {
+                    res.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                // Left column
+                for (int i = bottom; i >= top; i--) {
+                    res.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return res;
+    }
+}
